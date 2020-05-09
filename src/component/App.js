89f6f07afe667392aea1c9d341353e78d6d4107f
TakuoIshida <archersparadox.100@gmail.com>
2020-05-09 14:10:02 +0900

    How to dispatcher,useeducer

--- src/component/App.js
+++ src/component/App.js
@@ -1,20 +1,40 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-const App = props => {
+import reducer from '../reducers'
+import Event from './Event'
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+    dispatch({
+        type:'CREATE_EVENT',
+        title,
+        body
+    })
+    setTitle('')
+    setBody('')
+  }
+
+ 
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
         <label htmlFor="formEventTitle">タイトル</label>
-        <input className="form-control" id="formEventTitle"></input>
+        <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}></input>
         </div>
         <div className="form-group">
         <label htmlFor="formEventBody">ボディー</label>
-        <input className="form-control" id="formEventBody"></input>
+        <input className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}></input>
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-warning">イベントを全て削除する</button>
         {/* <button className="btn btn-info">ログを削除する</button> */}
       </form>
@@ -29,6 +49,7 @@ const App = props => {
           </tr>
         </thead>
         <tbody>
+          {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
         </tbody>
       </table>
     </div>
---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+const events = (state =[], action) => {
+    switch(action.type) {
+        case 'CREATE_EVENT':
+            const event = { title: action.title, body: action.body }
+            const length = state.length
+            const id = length === 0? 1 : state[length-1].id + 1
+            // ...でstateを全て取得し、その後ろにid: idとそのeventJSONを全て取得する
+            // return [...state, { id: id, ...event}]
+            return [...state, { id, ...event}]
+        case 'DELETE_EVENT':
+            return state.filter(event => event.id !== action.id)
+        case 'DELETE_ALL_EVENT':
+            return []
+        default:
+            return state
+    }
+}
+export default events
